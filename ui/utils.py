0cc50bb38fa20722b4bf81cc3eefa9162be77fac
Joshua Liu <joshua159230@gmail.com>
2020-08-09 21:17:15 -0700

    GUI checkboxes

---
+++ ui/utils.py
@@ -0,0 +1,11 @@
+import os
+import csv
+
+''' Helpers '''
+
+def getCategoryNames():
+    categories = []
+    with open('../lists/list_ids.csv', mode='r') as file:
+        csvList = list(csv.reader(file,delimiter=','))[1:]
+        categories = [x for [x,_] in csvList]
+    return categories
