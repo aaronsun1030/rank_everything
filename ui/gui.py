a432c27dc4837a06d8d14424f0f64375ce643371
Joshua Liu <joshua159230@gmail.com>
2020-08-15 12:14:42 -0700

    gui with scroll windows

--- ui/gui.py
+++ ui/gui.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QLineEdit, QLabel, QPushButton, QMessageBox, QCheckBox
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 
 import numpy as np
@@ -8,6 +8,7 @@ import os
 import csv
 
 from utils import *
+from main_window_frames import *
 
 def e():
     sys.exit()
@@ -21,51 +22,98 @@ class App(QWidget):
         self.top = 100
         self.width = 1600
         self.height = 900
-        # self.buttonClicks = 0
-        
-        self.categories = getCategoryNames()
-        self.categoriesCheckBoxes = self.categories.copy()
-        print(self.categories)
-
         self.initUI()
-
     
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        # self.grid = QGridLayout()
 
-        ''' Labels '''
-        self.label = QLabel(self)
-        # self.label.setText("Clicks: " + str(self.buttonClicks))
-        # self.label.move(30,0)
-       
-        ''' Buttons '''
-        self.selectCategories = QPushButton('Select Categories', self)
-        self.selectCategories.move(100,100)
-        self.selectCategories.clicked.connect(self.checkboxChanged)
+        formLayout1 = QFormLayout()
+        formLayout2 = QFormLayout()
+        groupBox1 = QGroupBox("Group Box 1")
+        groupBox2 = QGroupBox("Group Box 2")
         
-        self.show_text = QPushButton('Show Text', self)
-        self.show_text.clicked.connect(self.on_click_show_text)
-        self.show_text.move(500,500)
-
-        ''' Textbox '''
-        self.textbox = QLineEdit(self)
-        self.textbox.setText('Initial text')
-        self.textbox.move(500,600)
-
-        ''' Checkbox '''
-        self.createCheckboxes()
+        labelList1 = []
+        comboList1 = []
+        for i in range(30):
+            labelList1.append(QLabel("Label"))
+            comboList1.append(QPushButton("Click Me"))
+            formLayout1.addRow(labelList1[i], comboList1[i])
+        groupBox1.setLayout(formLayout1)
         
-        ''' Layout '''
-        # self.grid.addWidget(self.label,     0, 0, alignment=Qt.AlignCenter)
-        # self.grid.addWidget(self.check_box, 1, 0, alignment=Qt.AlignCenter)
-        # self.grid.addWidget(self.button,    2, 0, alignment=Qt.AlignCenter)
-        # self.grid.addWidget(self.show_text, 2, 1, alignment=Qt.AlignCenter)
-        # self.grid.addWidget(self.textbox,   3, 0, alignment=Qt.AlignCenter)
-        # self.setLayout(self.grid)
+        labelList2 = []
+        comboList2 = []
+        for i in range(30):
+            labelList2.append(QLabel("Label"))
+            comboList2.append(QPushButton("Click Me"))
+            formLayout2.addRow(labelList2[i], comboList2[i])
+        groupBox2.setLayout(formLayout2)
+        
+        scroll1 = QScrollArea()
+        scroll1.setWidget(groupBox1)
+        scroll1.setWidgetResizable(True)
+        scroll1.setFixedHeight(900)
+        scroll1.setFixedWidth(500)
 
+        scroll2 = QScrollArea()
+        scroll2.setWidget(groupBox2)
+        scroll2.setWidgetResizable(True)
+        scroll2.setFixedHeight(900)
+        scroll2.setFixedWidth(500)
+        
+        
+        layout = QHBoxLayout(self)
+        layout.addWidget(scroll1)
+        layout.addWidget(scroll2)
         self.show()
+
+
+
+
+
+        # ''' Labels '''
+        # self.currentLists = QLabel('Current Lists', self)
+        # self.currentLists.move(25,25)
+       
+        # ''' Buttons '''
+        # self.selectCategories = QPushButton('Select Categories', self)
+        # self.selectCategories.move(25,50)
+        # self.selectCategories.clicked.connect(self.checkboxChanged)
+        
+        # self.show_text = QPushButton('Show Text', self)
+        # self.show_text.clicked.connect(self.on_click_show_text)
+        # self.show_text.move(500,500)
+
+        # ''' Textbox '''
+        # self.textbox = QLineEdit(self)
+        # self.textbox.setText('Initial text')
+        # self.textbox.move(500,600)
+
+        # ''' Checkbox '''
+        # self.createCheckboxes()
+        
+        # ''' Scroll Area '''
+        # self.scroll_area = QScrollArea()
+        # self.scroll_area.setFixedWidth(250)
+        # self.scroll_area.setWidgetResizable(True)
+        
+        # widget = QWidget()
+        # self.scroll_area.setWidget(widget)
+        # self.layout_scroll_area = QVBoxLayout(widget)
+        # self.layout_scroll_area.addWidget(self.show_text)
+        
+        # ''' Layout '''
+        # # self.grid.addWidget(self.check_box, 1, 0, alignment=Qt.AlignCenter)
+        # # self.grid.addWidget(self.button,    2, 0, alignment=Qt.AlignCenter)
+        # # self.grid.addWidget(self.show_text, 2, 1, alignment=Qt.AlignCenter)
+        # # self.grid.addWidget(self.textbox,   3, 0, alignment=Qt.AlignCenter)
+        # # self.grid.addWidget(self.scroll_area, 4, 0, alignment=Qt.AlignCenter)
+        # # self.setLayout(self.grid)
+        # self.layout = QVBoxLayout()
+        # self.layout.addWidget(self.scroll_area)
+        # self.setLayout(self.layout)
+        
+        # self.show()
 
     def on_click(self):
         print('button clicked')
@@ -102,9 +150,7 @@ class App(QWidget):
         for i, v in enumerate(self.categories):
             self.categoriesCheckBoxes[i] = QCheckBox(v, self)
             self.categoriesCheckBoxes[i].move(500,base)
-            base += 25
-            print(self.categoriesCheckBoxes)
-            
+            base += 25            
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
